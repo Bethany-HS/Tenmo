fe8decdc74c624232c4458689002f0976bf3495e
Phil Scott <Phillipascott@gmail.com>
2021-03-03 15:44:50 -0500

    GetBalance client side

---
+++ TenmoClient/BaseService.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenmoClient
+{
+    public abstract class BaseService
+    {
+        public bool ProcessResponse(IRestResponse response)
+        {
+            if(response.ResponseStatus != ResponseStatus.Completed)
+            {
+                return false; 
+            }else if (!response.IsSuccessful)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
